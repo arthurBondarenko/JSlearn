0bd71cfb32a2057e1d0ff24ad16072e4976b1543
Arthur Bondarenko <artur36@inbox.lv>
2021-06-13 22:30:59 +0300

    Add flashlight

---
+++ flashlight/app.js
@@ -0,0 +1,6 @@
+let light = document.querySelector(".lantern");
+document.addEventListener("mousemove", function(e) {
+    let x = e.pageX;
+    let y = e.pageY;
+    light.style.background = "radial-gradient(circle at "+x+"px "+y+'px, transparent,#000 50%)';
+})
