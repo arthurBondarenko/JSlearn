e75435b6cba876b8a630d79977b5c27b747fdfa9
Arthur Bondarenko <artur36@inbox.lv>
2021-09-15 16:45:18 +0300

    Add musicPlayer

---
+++ musicPlayer/music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [
+  {
+    name: "Help me",
+    artist: "Name Surname",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Best of the best",
+    artist: "Audio Hujaudio",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Awesome name",
+    artist: "Peace & Duke",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "My recomendation",
+    artist: "aaaaaaaaaaaa",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Something Else",
+    artist: "From somewhere",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "WTF",
+    artist: "WTF Edition",
+    img: "music-6",
+    src: "music-6"
+  },
+
+];
