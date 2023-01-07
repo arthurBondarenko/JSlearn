203e7ed07f12ed14263c9d4ea5573700d5946eb0
Arthur Bondarenko <artur36@inbox.lv>
2023-01-07 20:51:10 +0200

    add Tank project

---
+++ tank/app.js
@@ -0,0 +1,51 @@
+let tank = document.querySelector(".tank");
+let soundMove = document.querySelector(".sound-move");
+let soundStop = document.querySelector(".sound-stop");
+let w = document.body.clientWidth;
+let h = document.body.clientHeight;
+
+window.addEventListener("resize", function () {
+  w = document.body.clientWidth;
+  h = document.body.clientHeight;
+});
+
+document.addEventListener("keydown", move);
+document.addEventListener("keyup", stop);
+
+function stop(e) {
+  soundMove.pause();
+  soundStop.currentTime = 0;
+  soundStop.play();
+}
+
+function move(e) {
+  soundStop.pause();
+  soundMove.play();
+  let key = e.key;
+  let x = tank.offsetLeft;
+  let y = tank.offsetTop;
+
+  if (key == "ArrowRight") {
+    tank.className = "tank";
+    tank.classList.add("right");
+    if (x < w - 150) tank.style.left = x + 10 + "px";
+  }
+
+  if (key == "ArrowDown") {
+    tank.className = "tank";
+    tank.classList.add("down");
+    if (y < h - 150) tank.style.top = y + 10 + "px";
+  }
+
+  if (key == "ArrowLeft") {
+    tank.className = "tank";
+    tank.classList.add("left");
+    if (x > 0) tank.style.left = x - 10 + "px";
+  }
+
+  if (key == "ArrowUp") {
+    tank.className = "tank";
+    tank.classList.add("up");
+    if (y > 0) tank.style.top = y - 10 + "px";
+  }
+}
